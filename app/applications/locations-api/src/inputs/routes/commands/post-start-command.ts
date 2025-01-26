8308dff191d54083434b32f90eb50af61c823245
Simon Norman <01secn@gmail.com>
2025-01-26 20:00:53 +0000

    feat: added stripe and cpo client

--- app/applications/locations-api/src/inputs/routes/commands/post-start-command.ts
+++ app/applications/locations-api/src/inputs/routes/commands/post-start-command.ts
@@ -1,16 +1,19 @@
 import type { FastifyTypebox } from "@breeze32/ts-backend-utilities";
-import { payments, stripeDefaultMeta } from "src/services/payments";
+import { deps } from "src/services/deps";
+import { stripeDefaultMeta } from "src/services/payments";
 
 export const postStartCommand = async (fastify: FastifyTypebox) => {
 	fastify.post("/start", {
 		handler: async () => {
-			await payments.paymentIntents.create({
+			await deps.payments.paymentIntents.create({
 				payment_method: "pm_card_visa",
 				amount: 500,
 				currency: "gbp",
 				confirm: true,
 				...stripeDefaultMeta(),
 			});
+
+			await deps.cpo.startCharge("chargerId");
 
 			return {
 				message: "Command started",
--- app/applications/locations-api/src/services/cpo.ts
+++ app/applications/locations-api/src/services/cpo.ts
@@ -1,7 +1,11 @@
 import { BaseClient } from "@breeze32/ts-backend-utilities";
 
 export class Cpo extends BaseClient {
-	async startCharge() {
-		await this.request({ method: "POST", url: "/commands/start" });
+	async startCharge(chargerId: string) {
+		await this.request({
+			method: "POST",
+			url: "/commands/start",
+			data: { chargerId },
+		});
 	}
 }
