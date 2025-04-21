0e208c9ccc1819d52f44a9e68a9bbbf7d58f06e1
Simon Norman <01secn@gmail.com>
2025-04-21 19:01:20 +0100

    fixes

--- app/applications/scripts/commands/locations-api/post-command.ts
+++ app/applications/scripts/commands/locations-api/post-command.ts
@@ -1,7 +1,11 @@
+import { initClient } from "@breeze32/locations-clients"
 import { Command } from "@oclif/core";
 
 export default class PostCommand extends Command {
-	public async run(): Promise<void> {
-		this.log("Hello from oclif!");
+	public async run() {
+		const { locationsSdk } = initClient({
+      endpoint: "https://locations-api.breeze32.dev",
+      username: "admin",
+      password
 	}
 }
